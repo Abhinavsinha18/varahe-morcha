0aa1372868943434f091634e1f3a15c88c22903e
Abhinav Sinha <abhinavraj350@gmail.com>
2024-09-04 06:10:40 +0530

    donr

--- database/db.js
+++ database/db.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 // let connect =  mongoose.connect("mongodb+srv://abhinav:abhinav@varahe.etletzy.mongodb.net/?retryWrites=true&w=majority&appName=varahe",{ useNewUrlParser: true, useUnifiedTopology: true });
-let connect =  mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+let connect =  mongoose.connect(process.env.MONGO_URL);
 module.exports = connect;
